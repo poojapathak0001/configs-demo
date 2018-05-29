b40f56c5271a7ea4b0311b3abb183791a159af86
Pooja Pathak <poopatha@publicisgroupe.net>
2018-05-29 17:10:33 +0530

    modified config file location

--- src/main/java/org/demo/exclude_resources_demo/App2.java
+++ src/main/java/org/demo/exclude_resources_demo/App2.java
@@ -13,7 +13,7 @@ public class App2 {
 	//Scanner sc = new Scanner(System.in);
 	try {
 
-		input = new FileInputStream("gs://cdp-gcp-script/config.properties");
+		input = new FileInputStream("\\D:\\config\\config.properties");
 		//System.out.println(sc.next());
 		// load a properties file
 		prop.load(input);
