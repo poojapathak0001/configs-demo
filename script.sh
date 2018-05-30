7ff1a3c2446d2073b3cae729a2ba78e139cea5cd
Pooja Pathak <poopatha@publicisgroupe.net>
2018-05-30 17:11:49 +0530

    trial

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ gcloud auth activate-service-account --key-file ./cdp-demo-d55fc085d0a7.json
 export PROJECT=$(gcloud info --format='value(config.project)')
 gsutil mb gs://cdp-demo-205604-jenkins-artifacts
 
-mvn clean install test >> gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER
+mvn clean install test >> gsutil gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER
 
-gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER
+gcloud gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER
